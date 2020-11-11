701462e69885a2e946085817cd029e2bf4fbb69c
rahul4989 <36290083+rahul4989@users.noreply.github.com>
2020-11-11 13:34:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function renderJoke(event){
     event.preventDefault()
 var fname=document.getElementById("firstname").value
 var lname=document.getElementById("lastname").value
-var apii='http://api.icndb.com/jokes/random?firstName='+fname+'&lastName='+lname
+var apii='https://api.icndb.com/jokes/random?firstName='+fname+'&lastName='+lname
 
 fetch(apii)
   .then(response => response.json())
